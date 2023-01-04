5ea765daaf5d88607bcce9581dfa8d8c54d56b20
hollowdll <pixerchat@gmail.com>
2023-01-04 12:44:38 +0200

    ready for prototyping

--- src/main.ts
+++ src/main.ts
@@ -1,15 +1,37 @@
-const {app, BrowserWindow} = require('electron');
+import {app, BrowserWindow} from "electron";
+import path from "path";
 
 const createWindow = () => {
     const win = new BrowserWindow({
       width: 800,
-      height: 600
+      height: 600,
+      webPreferences: {
+        preload: path.join(__dirname, "preload.js"),
+      },
     });
   
-    win.loadFile('index.html');
+    win.loadFile(path.join(process.cwd(), "src/html/index.html"));
+    win.webContents.openDevTools();
 }
 
+// This method will be called when Electron has finished
+// initialization and is ready to create browser windows.
+// Some APIs can only be used after this event occurs.
 app.whenReady().then(() => {
-    createWindow()
+    createWindow();
+
+    app.on('activate', () => {
+      // On macOS it's common to re-create a window in the app when the
+      // dock icon is clicked and there are no other windows open.
+      if (BrowserWindow.getAllWindows().length === 0) {
+        createWindow();
+      }
+    })
 });
 
+// Quit when all windows are closed, except on macOS
+app.on('window-all-closed', () => {
+  if (process.platform !== 'darwin') {
+    app.quit();
+  }
+})
