c633feb75c286cf733aba8b6be878d917ce5f1fb
hollowdll <pixerchat@gmail.com>
2023-01-05 12:51:38 +0200

    add debug window

--- src/main.ts
+++ src/main.ts
@@ -1,56 +1,89 @@
 import {app, BrowserWindow, ipcMain, nativeTheme} from "electron";
 import path from "path";
 
-const createMainWindow = () => {
-    const win = new BrowserWindow({
-      width: 800,
-      height: 600,
-      webPreferences: {
-        preload: path.join(__dirname, "preload.js"),
-      },
-    });
+const createMainWindow = (): BrowserWindow => {
+  const win = new BrowserWindow({
+    width: 800,
+    height: 600,
+    webPreferences: {
+      preload: path.join(__dirname, "preload.js"),
+    },
+    show: false
+  });
 
-    win.setBackgroundColor("rgb(50,50,50)");
-  
-    win.loadFile(path.join(process.cwd(), "src/html/index.html"));
+  win.once('ready-to-show', () => {
+    win.show()
+  });
 
-    // In development mode
-    win.webContents.openDevTools();
+  // win.setBackgroundColor("rgb(50,50,50)");
+
+  // When needed
+  // win.setFullScreen(true);
+
+  win.loadFile(path.join(process.cwd(), "src/html/index.html"));
+
+  // In development mode
+  win.webContents.openDevTools();
+
+  return win;
+}
+
+// Debug window for development
+// Contains buttons to test features
+const createDebugWindow = (mainWindow: BrowserWindow) => {
+  const win = new BrowserWindow({
+    width: 250,
+    height: 400,
+    webPreferences: {
+      preload: path.join(__dirname, "preload.js"),
+    },
+    show: false,
+    parent: mainWindow
+  });
+
+  win.once('ready-to-show', () => {
+    win.show()
+  });
+
+  // win.setBackgroundColor("rgb(50,50,50)");
+
+  win.loadFile(path.join(process.cwd(), "src/html/debug.html"));
 }
 
 ipcMain.handle('dark-mode:toggle', () => {
-    if (nativeTheme.shouldUseDarkColors) {
-      nativeTheme.themeSource = 'light';
-    } else {
-      nativeTheme.themeSource = 'dark';
-    }
-    return nativeTheme.shouldUseDarkColors;
+  if (nativeTheme.shouldUseDarkColors) {
+    nativeTheme.themeSource = 'light';
+  } else {
+    nativeTheme.themeSource = 'dark';
+  }
+  return nativeTheme.shouldUseDarkColors;
 });
 
 ipcMain.handle('dark-mode:system', () => {
-    nativeTheme.themeSource = 'system';
+  nativeTheme.themeSource = 'system';
 });
 
 // This method will be called when Electron has finished
 // initialization and is ready to create browser windows.
 // Some APIs can only be used after this event occurs.
 app.whenReady().then(() => {
-    createMainWindow();
+  const mainWindow = createMainWindow();
+  createDebugWindow(mainWindow);
 
-    app.on('activate', () => {
-      // On macOS it's common to re-create a window in the app when the
-      // dock icon is clicked and there are no other windows open.
-      if (BrowserWindow.getAllWindows().length === 0) {
-        createMainWindow();
-      }
-    })
+  app.on('activate', () => {
+    // On macOS it's common to re-create a window in the app when the
+    // dock icon is clicked and there are no other windows open.
+    if (BrowserWindow.getAllWindows().length === 0) {
+      createMainWindow();
+    }
+  });
 });
 
 // Quit when all windows are closed, except on macOS
 app.on('window-all-closed', () => {
-    if (process.platform !== 'darwin') {
-      app.quit();
-    }
+  if (process.platform !== 'darwin') {
+    app.quit();
+  }
 });
 
 
--- src/preload/renderer.d.ts
+++ src/preload/renderer.d.ts
@@ -1,13 +1,13 @@
 // Declare types for dark mode API
 // so renderer.ts can use it
 
-export interface IDarkModeAPI {
+export interface DarkMode {
     toggle: () => Promise<void>,
     system: () => Promise<void>,
 }
   
 declare global {
     interface Window {
-        darkMode: IDarkModeAPI,
+        darkMode: DarkMode,
     }
 }
